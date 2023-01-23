049a880b9b27207e93d21716038e2be333fcdb73
Edgar R. M <edgarrm358@gmail.com>
2023-01-23 13:39:28 -0600

    Update dependencies (#136)
    * chore: Update dependencies
    
    * Refactor OpenAPI logic
    
    * Update test workflow

--- tap_coda/client.py
+++ tap_coda/client.py
@@ -1,19 +1,17 @@
 """REST client handling, including CodaStream base class."""
 
-from pathlib import Path
-from typing import Any, Dict, Optional
+from __future__ import annotations
 
-from jsonschema.validators import RefResolver
-from singer.transform import _resolve_schema_references
+from typing import Any
+
 from singer_sdk.authenticators import BearerTokenAuthenticator
 from singer_sdk.streams import RESTStream
-from toolz.dicttoolz import get_in
-
-SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
 
 
 class CodaStream(RESTStream):
     """Coda stream class."""
+
+    openapi_ref: str
 
     url_base = "https://coda.io/apis/v1"
     records_jsonpath = "$.items[*]"
@@ -21,45 +19,44 @@ class CodaStream(RESTStream):
     primary_keys = ["id"]
     replication_key = None
 
-    @classmethod
-    def get_schema(cls, openapi: dict, resolver: RefResolver):
-        """Get schema from OpenAPI object and JSONSchema $ref resolver."""
-        schema = get_in(
-            [
-                "paths",
-                cls.path,
-                cls.rest_method.lower(),
-                "responses",
-                "200",
-                "content",
-                "application/json",
-                "schema",
-            ],
-            openapi,
-        )
-        resolved = _resolve_schema_references(schema, resolver)
-        return resolved["properties"]["items"]["items"]
-
     @property
     def authenticator(self) -> BearerTokenAuthenticator:
-        """Return a new authenticator object."""
+        """Return a new authenticator object.
+
+        Returns:
+            An authenticator for the Coda API.
+        """
         return BearerTokenAuthenticator.create_for_stream(
             self, token=self.config.get("auth_token")
         )
 
     @property
     def http_headers(self) -> dict:
-        """Return the http headers needed."""
+        """Return the http headers needed.
+
+        Returns:
+            A dictionary of HTTP headers.
+        """
         headers = {}
         if "user_agent" in self.config:
             headers["User-Agent"] = self.config.get("user_agent")
         return headers
 
     def get_url_params(
-        self, context: Optional[dict], next_page_token: Optional[str]
-    ) -> Dict[str, Any]:
-        """Return a dictionary of values to be used in URL parameterization."""
-        params = {
+        self,
+        context: dict | None,
+        next_page_token: str | None,
+    ) -> dict[str, Any]:
+        """Return a dictionary of values to be used in URL parameterization.
+
+        Args:
+            context: The stream context.
+            next_page_token: The next page value.
+
+        Returns:
+            A dictionary of URL query parameters.
+        """
+        params: dict[str, Any] = {
             "limit": 100,
         }
         if next_page_token:
--- tap_coda/streams.py
+++ tap_coda/streams.py
@@ -1,24 +1,42 @@
 """Stream type classes for tap-coda."""
 
-from typing import Optional
+from __future__ import annotations
+
+from typing import Any
 
 from tap_coda.client import CodaStream
 
 
 class Docs(CodaStream):
-    "Coda documents."
+    """Coda documents."""
 
     name = "docs"
     path = "/docs"
+    openapi_ref = "Doc"
 
-    def get_child_context(self, record: dict, context: Optional[dict]) -> dict:
+    def get_child_context(self, record: dict, context: dict | None) -> dict:
+        """Get context for docs child streams.
+
+        Args:
+            record: A `docs` record.
+            context: The stream context.
+
+        Returns:
+            Child stream context dictionary.
+        """
         return {"docId": record["id"]}
 
 
 class _DocChild(CodaStream):
     parent_stream_type = Docs
 
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        """Initialize a stream with `docs` as parent stream.
+
+        Args:
+            args: Positional arguments.
+            kwargs: Keyword arguments.
+        """
         super().__init__(*args, **kwargs)
         self._schema["properties"]["docId"] = {
             "type": "string",
@@ -27,51 +45,80 @@ class _DocChild(CodaStream):
 
 
 class Pages(_DocChild):
-    "Coda document pages."
+    """Coda document pages."""
 
     name = "pages"
     path = "/docs/{docId}/pages"
+    openapi_ref = "Page"
 
 
 class Controls(_DocChild):
-    "Coda document controls."
+    """Coda document controls."""
 
     name = "controls"
     path = "/docs/{docId}/controls"
+    openapi_ref = "ControlReference"
 
 
 class Formulas(_DocChild):
-    "Coda document pages."
+    """Coda document pages."""
 
     name = "formulas"
     path = "/docs/{docId}/formulas"
+    openapi_ref = "Formula"
 
 
 class Permissions(_DocChild):
-    "Coda document permissions."
+    """Coda document permissions."""
 
     name = "permissions"
     path = "/docs/{docId}/acl/permissions"
+    openapi_ref = "Permission"
 
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        """Initialize a stream with `docs` as parent stream.
+
+        Args:
+            args: Positional arguments.
+            kwargs: Keyword arguments.
+        """
         super().__init__(*args, **kwargs)
         self._schema["properties"]["principal"]["type"] = "object"
 
 
 class Tables(_DocChild):
-    "Coda document tables."
+    """Coda document tables."""
 
     name = "tables"
     path = "/docs/{docId}/tables"
+    openapi_ref = "Table"
 
-    def get_child_context(self, record: dict, context: Optional[dict]) -> dict:
-        return {"docId": context["docId"], "tableIdOrName": record["id"]}
+    def get_child_context(self, record: dict, context: dict | None) -> dict:
+        """Get context for tables child streams.
+
+        Args:
+            record: A `tables` record.
+            context: The stream context.
+
+        Returns:
+            Child stream context dictionary.
+        """
+        return {
+            "docId": context["docId"] if context else None,
+            "tableIdOrName": record["id"],
+        }
 
 
 class _TableChild(_DocChild):
     parent_stream_type = Tables
 
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        """Initialize a stream with `tables` as parent stream.
+
+        Args:
+            args: Positional arguments.
+            kwargs: Keyword arguments.
+        """
         super().__init__(*args, **kwargs)
         self._schema["properties"]["tableIdOrName"] = {
             "type": "string",
@@ -80,24 +127,38 @@ class _TableChild(_DocChild):
 
 
 class Columns(_TableChild):
-    "Coda document table columns."
+    """Coda document table columns."""
 
     name = "columns"
     path = "/docs/{docId}/tables/{tableIdOrName}/columns"
     parent_stream_type = Tables
+    openapi_ref = "Column"
 
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        """Initialize a stream with `docs` as parent stream.
+
+        Args:
+            args: Positional arguments.
+            kwargs: Keyword arguments.
+        """
         super().__init__(*args, **kwargs)
         self._schema["properties"]["format"]["type"] = "object"
 
 
 class Rows(_TableChild):
-    "Coda document table rows."
+    """Coda document table rows."""
 
     name = "rows"
     path = "/docs/{docId}/tables/{tableIdOrName}/rows"
     parent_stream_type = Tables
+    openapi_ref = "Row"
 
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        """Initialize a stream with `docs` as parent stream.
+
+        Args:
+            args: Positional arguments.
+            kwargs: Keyword arguments.
+        """
         super().__init__(*args, **kwargs)
         self._schema["properties"]["values"].pop("additionalProperties")
