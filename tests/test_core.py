c71164d9212473c55e2c61e01e0789a190d7e10b
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2023-11-09 10:57:23 -0600

    Ignore no records

--- tests/test_core.py
+++ tests/test_core.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 import datetime
 
-from singer_sdk.testing import get_tap_test_class
+from singer_sdk.testing import SuiteConfig, get_tap_test_class
 
 from tap_coda.tap import TapCoda
 
@@ -14,6 +14,9 @@ SAMPLE_CONFIG = {
 TestTapCoda = get_tap_test_class(
     TapCoda,
     config=SAMPLE_CONFIG,
+    suite_config=SuiteConfig(
+        ignore_no_records=True,
+    ),
     # TODO: Enable this test after the SDK handles AllOf properties.  # noqa: TD002, TD003, E501
     include_stream_attribute_tests=False,
 )
