25fc91a3cfee049c38c4e5ee047416930f01eb20
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2023-12-20 11:55:59 -0600

    test: Enable SDK feature tests
    Signed-off-by: Edgar Ramírez Mondragón <edgarrm358@gmail.com>

--- tests/test_core.py
+++ tests/test_core.py
@@ -4,16 +4,18 @@ from __future__ import annotations
 
 import datetime
 
-from singer_sdk.testing import get_standard_tap_tests
+from singer_sdk.testing import SuiteConfig, get_tap_test_class
 
 from tap_coda.tap import TapCoda
 
 SAMPLE_CONFIG = {"start_date": datetime.datetime.now(datetime.UTC).strftime("%Y-%m-%d")}
 
-
-# Run standard built-in tap tests from the SDK:
-def test_standard_tap_tests():
-    """Run standard tap tests from the SDK."""
-    tests = get_standard_tap_tests(TapCoda, config=SAMPLE_CONFIG)
-    for test in tests:
-        test()
+TestTapCoda = get_tap_test_class(
+    TapCoda,
+    config=SAMPLE_CONFIG,
+    suite_config=SuiteConfig(
+        ignore_no_records=True,
+    ),
+    # TODO: Enable this test after the SDK handles AllOf properties.  # noqa: TD002, TD003, E501
+    include_stream_attribute_tests=False,
+)
