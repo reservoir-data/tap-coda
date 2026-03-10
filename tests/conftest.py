014720639d9cf92b2e740f68e6892eee2d2da421
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2026-03-09 21:27:19 -0600

    test: Enable SDK feature tests (#173)
    * test: Enable SDK feature tests
    
    Signed-off-by: Edgar Ramírez Mondragón <edgarrm358@gmail.com>
    
    * Bump singer-sdk
    
    Signed-off-by: Edgar Ramírez Mondragón <edgarrm358@gmail.com>
    
    * Enable attr tests
    
    Signed-off-by: Edgar Ramírez Mondragón <edgarrm358@gmail.com>
    
    * Update snapshots
    
    Signed-off-by: Edgar Ramírez Mondragón <edgarrm358@gmail.com>
    
    * Modernize some more
    
    Signed-off-by: Edgar Ramírez Mondragón <edgarrm358@gmail.com>
    
    * Fixed XFAILs
    
    Signed-off-by: Edgar Ramírez Mondragón <edgarrm358@gmail.com>
    
    ---------
    
    Signed-off-by: Edgar Ramírez Mondragón <edgarrm358@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,4 +2,23 @@
 
 from __future__ import annotations
 
+import fnmatch
+
+import pytest
+
 pytest_plugins = "pytester"
+
+XFAIL_SCHEMA_MISMATCH = pytest.mark.xfail(reason="Schema mismatch against OpenAPI spec")
+SCHEMA_MISMATCH: set[str] = {
+    "test_tap_stream_record_matches_stream_schema[[]columns[]]",
+    "test_tap_stream_record_matches_stream_schema[[]rows[]]",
+    "test_tap_stream_record_matches_stream_schema[[]tables[]]",
+}
+
+
+def pytest_runtest_setup(item: pytest.Item) -> None:
+    """Skip tests that require a live API key."""
+    test_name = item.name.split("::")[-1]
+
+    if any(fnmatch.fnmatch(test_name, pattern) for pattern in SCHEMA_MISMATCH):
+        item.add_marker(XFAIL_SCHEMA_MISMATCH)
